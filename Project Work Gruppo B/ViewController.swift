429172733d3d7f916c739d8231ce5bb91ee21277
Eddi Raimondi <raimondi.eddy2010@libero.it>
2018-11-28 09:31:54 +0100

    Initial Commit

---
+++ Project Work Gruppo B/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Work Gruppo B
+//
+//  Created by Eddi Raimondi on 28/11/2018.
+//  Copyright © 2018 Eddi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
