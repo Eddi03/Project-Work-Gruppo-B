f9ccf42e4ae7c2a8e4ebbb61918e6126da6cb4b3
Eddi Raimondi <raimondi.eddy2010@libero.it>
2018-12-03 16:39:11 +0100

    Logout in minuscolo

--- Project Work Gruppo B/Authentication/LoginViewController.swift
+++ Project Work Gruppo B/Authentication/LoginViewController.swift
@@ -26,12 +26,9 @@ class LoginViewController: UIViewController {
         NetworkManager.login(email: email, password: password){ (success) in
             if success{
                 self.dismiss(animated: true, completion:{
-                    self.performSegue(withIdentifier: "", sender: self)})
+                    self.performSegue(withIdentifier: R.segue.loginViewController.segueToOptions, sender: self)})
             }
         }
-    }
-    @IBAction func goToRegister(_ sender: Any) {
-        self.performSegue(withIdentifier: "", sender: self)
     }
     
     @IBAction func resetP(_ sender: Any) {
