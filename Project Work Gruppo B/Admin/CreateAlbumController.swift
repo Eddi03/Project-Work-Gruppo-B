c310bde12dce9c977f1ead1877cb3f255f422416
Sarah <sarah.dalsanto@tecnicosuperiorekennedy.it>
2018-12-04 14:22:42 +0100

    create album

--- Project Work Gruppo B/Admin/CreateAlbumController.swift
+++ Project Work Gruppo B/Admin/CreateAlbumController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class CreateAlbumController: UIViewController {
 
+    var titleAlbum : String = ""
+    var infoAlbum : String = ""
+   
+    @IBOutlet weak var infoAlbumTextField: UITextField!
+    @IBOutlet weak var titleAlbumTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func avantiActionButton(_ sender: Any) {
+        titleAlbum = titleAlbumTextField.text ?? ""
+        infoAlbum = infoAlbumTextField.text ?? ""
+        
+        guard !titleAlbum.isEmpty && !infoAlbum.isEmpty else {
+            self.present(GeneralUtils.share.alertError(title: "Attenzione", message: "uno o più campi sono vuoti"), animated: true, completion: nil)
+            return
+        }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
