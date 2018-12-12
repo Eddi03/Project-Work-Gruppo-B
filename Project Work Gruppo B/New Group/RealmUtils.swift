09df1a23c29af36e48ebb47943d547cd25a526b8
Sarah <sarah.dalsanto@tecnicosuperiorekennedy.it>
2018-12-12 09:44:34 +0100

    corretto errore fatal

--- Project Work Gruppo B/New Group/RealmUtils.swift
+++ Project Work Gruppo B/New Group/RealmUtils.swift
@@ -3,7 +3,7 @@ import RealmSwift
 
 class RealmUtils {
     
-    private static var REALM_SCHEMA_VERSION : UInt64 = 4
+    private static var REALM_SCHEMA_VERSION : UInt64 = 5
     
     /// Setup Realm Configuration
     static var config: Realm.Configuration {
